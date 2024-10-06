57dd74dd0131bc83c14a7d5a3528d8fe3ef45282
bill <bill@hey.com>
2024-10-06 11:51:33 -0700

    fixed stops and operator requests

--- app/Http/Controllers/TransitController.php
+++ app/Http/Controllers/TransitController.php
@@ -42,15 +42,19 @@ class TransitController extends Controller
         $apiKey = env('511_API_KEY');
         $baseUrl = env('511_API_BASE_URL');
 
-        // Fetch the list of stops
-        $response = Http::get("{$baseUrl}/stops", [
+        $response = Http::withHeaders([
+            'Accept' => 'application/json',
+        ])->get("{$baseUrl}/stops", [
             'api_key' => $apiKey,
             'operator_id' => $operator,
         ]);
 
+        $cleanedResponse = preg_replace('/\x{FEFF}/u', '', $response->body());
+
         if ($response->successful()) {
-            return response()->json($response->json());
+            return response()->json(json_decode($cleanedResponse, true));
         } else {
+            \Log::error('Failed to fetch stop data', ['response' => $response->body()]);
             return response()->json(['error' => 'Unable to fetch stop data'], 500);
         }
     }
@@ -60,25 +64,25 @@ class TransitController extends Controller
      */
     public function getOperators()
     {
-        return Cache::remember('operators', now()->addDay(), function () {
-            $apiKey = env('511_API_KEY');
-            $baseUrl = env('511_API_BASE_URL');
+        $apiKey = env('511_API_KEY');
+        $baseUrl = env('511_API_BASE_URL');
 
-            try {
-                $response = Http::get("{$baseUrl}/operators", [
-                    'api_key' => $apiKey,
-                ]);
+        // Perform the API request
+        $response = Http::withHeaders([
+            'Accept' => 'application/json', // Specify JSON response format
+        ])->get("{$baseUrl}/operators", [
+            'api_key' => $apiKey,
+        ]);
 
-                if ($response->successful()) {
-                    return $response->json();
-                } else {
-                    \Log::error('Failed to fetch operator data', ['response' => $response->body()]);
-                    return ['error' => 'Unable to fetch operator data'];
-                }
-            } catch (\Exception $e) {
-                \Log::error('Exception occurred while fetching operator data', ['exception' => $e->getMessage()]);
-                return ['error' => 'Unable to fetch operator data'];
-            }
-        });
+        // Remove any BOM from the response body
+        $cleanedResponse = preg_replace('/\x{FEFF}/u', '', $response->body());
+
+        // Check if the response is successful
+        if ($response->successful()) {
+            return response()->json(json_decode($cleanedResponse, true)); // Return valid JSON response
+        } else {
+            \Log::error('Failed to fetch operator data', ['response' => $response->body()]);
+            return response()->json(['error' => 'Unable to fetch operator data'], 500);
+        }
     }
 }
